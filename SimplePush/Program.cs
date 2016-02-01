a68bff74a131667b3a9d82028ca22f3baaedbe87
Bryan Guerre <bguerre12@gmail.com>
2016-02-01 13:45:37 -0800

    Initial git commit to master, simple http server tutorial followed, can receive geet and post requests over local network

---
+++ SimplePush/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimplePush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string localAddress = GetLocalIPAddress();
+            Console.WriteLine(localAddress);
+            HttpServer httpServer = new SimpleHttpServer(8080);
+            Thread thread = new Thread(new ThreadStart(httpServer.listen));
+            thread.Start();
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+    }
+}
