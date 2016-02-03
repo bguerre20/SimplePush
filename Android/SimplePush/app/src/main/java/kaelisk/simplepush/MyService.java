de87901c99a3e663b5ec2b051a3dba161f98ee5d
Bryan Guerre <bguerre12@gmail.com>
2016-02-02 16:57:24 -0800

    Dirty, dirty test for proof of concept. Forced android permissions to allow networking in main thread of activity, but it works dammit. Sent a post request from phone to C# server and it received it.

--- Android/SimplePush/app/src/main/java/kaelisk/simplepush/MyService.java
+++ Android/SimplePush/app/src/main/java/kaelisk/simplepush/MyService.java
@@ -3,12 +3,31 @@ package kaelisk.simplepush;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyService extends Service {
+
     public MyService() {
 
-        Toast.makeText(MyService.this, "Service Started!", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(MyService.this, "Service Started!", Toast.LENGTH_SHORT).show();
+        //SmsListener smsL = new SmsListener();
+
+
     }
 
     @Override
